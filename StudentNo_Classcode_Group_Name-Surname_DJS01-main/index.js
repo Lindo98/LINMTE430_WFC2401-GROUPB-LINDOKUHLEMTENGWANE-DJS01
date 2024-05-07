7ccd2a7448eb272704f5edd94be6047d24eaf2cf
Lindokuhle Mtengwane <lindokuhlemtengwane@gmail.com>
2024-05-07 23:46:43 +0200

    added functions

--- StudentNo_Classcode_Group_Name-Surname_DJS01-main/index.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS01-main/index.js
@@ -13,31 +13,42 @@ const initialDistance = 0; // distance (km)
 const remainingFuel = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
-const d2 = (initialDistance + (velocityKmPerHour / 3.6) * timeSeconds) / 1000; //calcultes new distance
-const rf = remainingFuel - fuelBurnRate * timeSeconds; //calculates remaining fuel
-const vel2 = calcNewVel(
-  accelerationMetersPerSecSquared,
-  velocityKmPerHour,
-  timeSeconds
-); //calculates new velocity based on acceleration
-
 // Pick up an error with how the function below is called and make it robust to such errors
-function calcNewVel(
+const calcNewVel = (
   accelerationMetersPerSecSquared,
   velocityKmPerHour,
   timeSeconds
-) {
-  if (
-    typeof velocityKmPerHour !== "number" ||
-    typeof accelerationMetersPerSecSquared !== "number" ||
-    typeof velocityKmPerHour !== "number"
-  ) {
-    console.log("invalid input. Parameters must be numbers");
+) => {
+  if (accelerationMetersPerSecSquared < 1) {
+    throw new Error("Acceleration must be in meters per second squared.");
   }
   return (
     velocityKmPerHour + accelerationMetersPerSecSquared * 3.6 * timeSeconds
   );
-}
+};
+
+const calcDistance = (initialDistance, velocityKmPerHour, timeSeconds) => {
+  if (velocityKmPerHour == "km/h") {
+    throw new Error("Velocity must be converted to meters per second.");
+  }
+  return (initialDistance + (velocityKmPerHour / 3.6) * timeSeconds) / 1000;
+};
+
+const calcRemainingFuel = (remainingFuel, fuelBurnRate, timeSeconds) => {
+  if (remainingFuel < 0) {
+    throw new Error("Remaining fuel can not be 0");
+  }
+
+  return remainingFuel - fuelBurnRate * timeSeconds;
+};
+
+const d2 = calcDistance(initialDistance, velocityKmPerHour, timeSeconds);
+const rf = calcRemainingFuel(remainingFuel, fuelBurnRate, timeSeconds);
+const vel2 = calcNewVel(
+  accelerationMetersPerSecSquared,
+  velocityKmPerHour,
+  timeSeconds
+);
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);

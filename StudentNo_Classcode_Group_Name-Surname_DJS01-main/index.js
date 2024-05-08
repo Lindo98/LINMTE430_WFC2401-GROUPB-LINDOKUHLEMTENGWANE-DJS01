de1f744580abf71bb2ae30193e952540f48489b9
Lindokuhle Mtengwane <lindokuhlemtengwane@gmail.com>
2024-05-08 10:50:19 +0200

    added information on read me

--- StudentNo_Classcode_Group_Name-Surname_DJS01-main/index.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS01-main/index.js
@@ -1,19 +1,15 @@
-/**
- * Debugging Guide
- * 1. Make the code more readable
- * 2. Pick up calculation errors
- * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
- */
+// I have named my variables using full names to clearly indicate what they are for
 
-// Given Parameters
-const velocityKmPerHour = 10000; // velocity (km/h)
-const accelerationMetersPerSecSquared = 3; // acceleration (m/s^2)
-const timeSeconds = 3600; // seconds (1 hour)
-const initialDistance = 0; // distance (km)
-const remainingFuel = 5000; // remaining fuel (kg)
-const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
+const velocityKmPerHour = 10000;
+const accelerationMetersPerSecSquared = 3;
+const timeSeconds = 3600;
+const initialDistance = 0;
+const remainingFuel = 5000;
+const fuelBurnRate = 0.5;
 
-// Pick up an error with how the function below is called and make it robust to such errors
+// This is a function to calculate a new velocity, I used function expression
+// I declared the calcNewVel then assigned it an arrow function then added some parameters
+//
 const calcNewVel = (
   accelerationMetersPerSecSquared,
   velocityKmPerHour,
@@ -27,7 +23,7 @@ const calcNewVel = (
   );
 };
 
-const calcDistance = (initialDistance, velocityKmPerHour, timeSeconds) => {
+const calcNewDistance = (initialDistance, velocityKmPerHour, timeSeconds) => {
   if (velocityKmPerHour == "km/h") {
     throw new Error("Velocity must be converted to meters per second.");
   }
@@ -36,13 +32,13 @@ const calcDistance = (initialDistance, velocityKmPerHour, timeSeconds) => {
 
 const calcRemainingFuel = (remainingFuel, fuelBurnRate, timeSeconds) => {
   if (remainingFuel < 0) {
-    throw new Error("Remaining fuel can not be 0");
+    throw new Error("Need more fuel");
   }
 
   return remainingFuel - fuelBurnRate * timeSeconds;
 };
 
-const d2 = calcDistance(initialDistance, velocityKmPerHour, timeSeconds);
+const d2 = calcNewDistance(initialDistance, velocityKmPerHour, timeSeconds);
 const rf = calcRemainingFuel(remainingFuel, fuelBurnRate, timeSeconds);
 const vel2 = calcNewVel(
   accelerationMetersPerSecSquared,
